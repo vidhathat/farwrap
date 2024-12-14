771b5ae8a6e7632862cbc06fba5046fc6c64c5fa
Your Vidhatha <vtvidhathatallapally@gmail.com>
2024-12-14 23:36:52 +0530

    modify share text

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -100,7 +100,7 @@ function generateWarpcastComposeURL(text: any, mentions = [], username: string) 
 
   // Append mentions after two line breaks if any exist
   const textWithMentions = formattedMentions.length > 0 
-    ? `${shareText}\nTop engaged fam : ${formattedMentions.map(m => `@${m}`).join(', ')}\n\nFrame built by @Vidhatha @Devesh` 
+    ? `${shareText}\nTop engaged fam : ${formattedMentions.map(m => `@${m}`).join(', ')}\n\nFrame built by @Vidhatha` 
     : shareText;
 
   // Encode the text
